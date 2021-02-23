26a150b9d9d679436082b3ad7df87eb99725f7db
Pedro Faria <pedrobentodefaria@gmail.com>
2021-02-23 14:48:35 -0300

    controller de adoptions v.1.0 feito, respectivas rotas implementadas

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,2 +1,25 @@
 class AdoptionsController < ApplicationController
+
+  def new
+    @pet = Pet.find(params[:pet_id])
+    @user = User.find(params[:user_id])
+    @adoption = Adoption.new
+  end
+
+  def create
+    @adoption = Adoption.new(adoption_params)
+
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def all_adoptions
+    @all_adoptions = Adoption.where(user: params[:user_id])
+  end
+
+  private
+
+  def params
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  resources :pets do
+    resources :adoptions, only: [:new, :create]
+  end
+
+  resources :users, only: [:show] do
+    collection do
+      get :all_adoptions
+    end
+  end
+
 end
