3a8c47bf8ab17f136d0e4724fd2f3d5ab627af21
Pedro Faria <pedrobentodefaria@gmail.com>
2021-02-24 14:32:02 -0300

    corrected message field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_172622) do
+ActiveRecord::Schema.define(version: 2021_02_24_173014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2021_02_24_172622) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "concluded", default: false
     t.date "concluded_at"
-    t.text "message", null: false
     t.index ["pet_id"], name: "index_adoptions_on_pet_id"
     t.index ["user_id"], name: "index_adoptions_on_user_id"
   end
