a606bb8d45d6b765d739b0018bd0a3f646204851
Pedro Faria <pedrobentodefaria@gmail.com>
2021-02-25 22:17:00 -0300

    added migration removing rating from adoptions

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -8,7 +8,8 @@ class AdoptionsController < ApplicationController
     @adoption = Adoption.new(adoption_params)
     @adoption.user = current_user
     @adoption.pet = Pet.find(params[:pet_id])
-    if @adoption.save
+    @adoption.save
+    if @adoption.save?
       mail = UserMailer.with(pet: @adoption.pet, adoption: @adoption).adoption
       mail.deliver_now
       redirect_to pet_path(@adoption.pet), notice: "Adoption created, message sent"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_234541) do
+ActiveRecord::Schema.define(version: 2021_02_26_011418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2021_02_25_234541) do
   create_table "adoptions", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "pet_id", null: false
-    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "concluded", default: false
